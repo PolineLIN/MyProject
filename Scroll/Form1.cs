5689c21b54bf7c15ef1c5544922e1ca9a7fbbeaa
unknown <poline96225@gmail.com>
2019-03-30 12:17:26 +0800

    add my sourcecdoe

---
+++ Scroll/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Scroll
+{
+    public partial class Form1 : Form
+    {
+        private Form2 form2;
+        public Form1()
+        {
+            InitializeComponent();
+            //將Form1設定成容器
+            this.IsMdiContainer = true;
+            //將Form1最大化
+            this.WindowState = FormWindowState.Maximized;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //建立一個新Form2物件
+            form2 = new Form2(this);
+            //將Form2放在Form1內
+            form2.MdiParent = this;
+            form2.Show();
+        }
+        public void SetValue(Point P1, Point P2)
+        {
+            toolStripStatusLabel1.Text = "AutoScrollPosition = " + P1.ToString();
+            toolStripStatusLabel2.Text = "ScrollPosition = " + P2.ToString();
+        }
+    }
+}
---
+++ Scroll/Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Scroll
+{
+    public partial class Form2 : Form
+    {
+        private Form1 F1;
+        private int PaintCount = 0;
+        private int ScrollCount = 0;
+
+        public Form2(Form1 F1)
+        {
+            InitializeComponent();
+            this.F1 = F1;
+            this.DoubleBuffered = true;
+            //this.AutoScroll = true;
+            //this.AutoScrollMinSize = new Size(10000, 10000);
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form2_Paint(object sender, PaintEventArgs e)
+        {
+            F1.SetValue(this.AutoScrollPosition, this.AutoScrollOffset);
+            this.PaintCount++;
+            this.Text = "Panit:" + PaintCount.ToString() + "Scroll:" + ScrollCount.ToString();
+        }
+        private Point ScrollP1;
+
+        private void Form2_Scroll(object sender, ScrollEventArgs e)
+        {
+
+            this.ScrollCount++;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.textBox1.Select(Str.Length, 1);
+
+            this.textBox1.Text = Str;
+
+            //this.textBox1.ScrollToCaret();
+        }
+        string Str = "基準點座標(@X,Y設原點/X,Y相對座標/LEN;{ANG;Y/X}角度)：";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = Str;
+            this.textBox1.Select(Str.Length, 1);
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //MessageBox.Show(e.KeyValue.ToString());
+        }
+    }
+}
